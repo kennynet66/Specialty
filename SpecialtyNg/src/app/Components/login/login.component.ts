2d41ee5c0adffc195dcca7e425f03ec96d0a7714
Kenedy Maina <kennynet66@gmail.com>
2024-03-12 00:43:59 +0300

    Login + register page

--- SpecialtyNg/src/app/Components/login/login.component.ts
+++ SpecialtyNg/src/app/Components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, RouterLink, ReactiveFormsModule, CommonModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm!: FormGroup;
+  errorDiv = false;
+  successDiv = false;
+  errorMsg!: string;
+  successMsg!: string;
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
 }
--- SpecialtyNg/src/app/Components/register/register.component.ts
+++ SpecialtyNg/src/app/Components/register/register.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ NavbarComponent, CommonModule, ReactiveFormsModule, RouterLink ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  errorMsg!: string;
+  successMsg!: string;
+  errorDiv= false;
+  successDiv = false
+  registerForm!: FormGroup;
 
 }
