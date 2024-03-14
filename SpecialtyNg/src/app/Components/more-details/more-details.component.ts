0607c575d4209a777f1233857ae78222ed6ca1d3
Kenedy Maina <kennynet66@gmail.com>
2024-03-14 15:17:10 +0300

    Styled user-details form

--- SpecialtyNg/src/app/Components/more-details/more-details.component.ts
+++ SpecialtyNg/src/app/Components/more-details/more-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-more-details',
   standalone: true,
-  imports: [],
+  imports: [ FooterComponent ],
   templateUrl: './more-details.component.html',
   styleUrl: './more-details.component.css'
 })
